08ed539d96267a6cfda44fe5649de9c4e8365e19
user <siriteanucristina25@gmail.com>
2022-04-21 11:24:32 +0300

    The TypeScript types

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 //The number type
+// 1.
 var price = 9.95;
 console.log("Pretul este:", price);
+// 2.
 var counter = 50;
 var x = 100, y = 200;
-console.log(counter, x + y);
+console.log(counter, x + y, y - x);
+//The string type
+// 1.
 var firstName = "Carl";
 var secondName = "Stooddard";
 var fullName = "His name is ".concat(firstName).concat(secondName, ".");
 console.log(fullName);
-//The string type
+// 2.
 var message = "Hello, World!";
-// console.log(message);
 var heading = document.createElement("h1");
 heading.textContent = message;
 document.body.appendChild(heading);
-//The boolean type
 //The object type
+var list;
+list = {
+    one: "E",
+    two: "B",
+    three: "S"
+};
+console.log(list);
 //The array type
-//The tuple type
+// 1.
+var books = ["Se numea Sarah", "Persuasiune"];
+console.log("Carti preferate:", books);
+// 2.
+var pow = [0, 2, 4, 6];
+var powArray = pow.map(function (e) { return e * e; });
+console.log(powArray);
+// 3.
+var combination;
+combination = [1, "One", 2, "Two", 3, "Three"];
+console.log(combination);
+//Functii
 function add(x, y) {
     return x + y;
 }
--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ console.log("Pretul este:", price);
 let counter: number = 50;
 let x: number = 100,
   y: number = 200;
-console.log(counter, x + y);
+console.log(counter, x + y, y - x);
 
 //The string type
 // 1.
@@ -21,14 +21,29 @@ let heading = document.createElement("h1");
 heading.textContent = message;
 document.body.appendChild(heading);
 
-//The boolean type
-
 //The object type
+let list: object;
+list = {
+  one: "E",
+  two: "B",
+  three: "S",
+};
+console.log(list);
 
 //The array type
+// 1.
+let books = ["Se numea Sarah", "Persuasiune"];
+console.log("Carti preferate:", books);
+// 2.
+let pow = [0, 2, 4, 6];
+let powArray = pow.map((e) => e * e);
+console.log(powArray);
+// 3.
+let combination: (string | number)[];
+combination = [1, "One", 2, "Two", 3, "Three"];
+console.log(combination);
 
-//The tuple type
-
+//Functii
 function add(x: number, y: number) {
   return x + y;
 }
